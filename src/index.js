2c5474757a79a9af19e94a027974fa94810b2c10
alWestDeden <baboumediene@laposte.net>
2023-11-20 10:57:14 +0100

    update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals"
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
 	<React.StrictMode>
-		<App basename='/77events' />
+		<App />
 	</React.StrictMode>
 )
 
